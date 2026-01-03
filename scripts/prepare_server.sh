5aca96a2315f8c8a1a341b13e63d1cae8a6d8859
Christian Pfeiffer <cpfeiffer@rev-crew.info>
2026-01-03 01:43:05 +0100

    prepare: Add mods linking

--- scripts/prepare_server.sh
+++ scripts/prepare_server.sh
@@ -5,28 +5,30 @@ steamcmd +force_install_dir $1 +login anonymous +app_update 462310 validate +qui
 cd $1
 git -C doi/cfg/doi-config pull
 
-# Create config symlinks
-for gameconfig in doi/cfg/doi-config/configs/gamemodes/*.cfg
-do
-    gameconfig_file="${gameconfig##*/}"
-    ln -sf doi-config/configs/gamemodes/$gameconfig_file doi/cfg/$gameconfig_file
-done
-
 # Apply server playlist unlock patch
 cp doi/cfg/doi-config/server-patch/server_srv.so doi/bin/server_srv.so
-
-# Link included sourcemod scripts
-for smscript in doi/cfg/doi-config/sourcemod/*.sp
-do
-    smscript_file="${smscript##*/}"
-    ln -sf ../../../cfg/doi-config/sourcemod/$smscript_file doi/addons/sourcemod/scripting/$smscript_file
-done
 
 # Remove the shipped, outdated standard libraries
 rm bin/libgcc_s.so.1
 rm bin/libstdc++.so.6
 rm bin/libogg.so
 rm bin/libvorbis.so
+
+# Add included mods
+mkdir -p doi/custom
+for modfolder in doi/cfg/doi-config/mods/*/
+do
+    modfolder_path="${modfolder%*/}"
+    modfolder_name="${modfolder_path##*/}"
+    ln -sf ../cfg/doi-config/mods/$modfolder_name doi/custom/$modfolder_name
+done
+
+# Create config symlinks
+for gameconfig in doi/cfg/doi-config/configs/gamemodes/*.cfg
+do
+    gameconfig_file="${gameconfig##*/}"
+    ln -sf doi-config/configs/gamemodes/$gameconfig doi/cfg/$gameconfig_file
+done
 
 # Update metamod source and sourcemod
 METAMOD_BRANCH="1.12"
@@ -50,6 +52,13 @@ tar -xf sourcemod.tgz -C doi --exclude="configs" --exclude="cfg"
 rm metamod.tgz
 rm sourcemod.tgz
 
+# Link included sourcemod scripts
+for smscript in doi/cfg/doi-config/sourcemod/*.sp
+do
+    smscript_file="${smscript##*/}"
+    ln -sf ../../../cfg/doi-config/sourcemod/$smscript_file doi/addons/sourcemod/scripting/$smscript_file
+done
+
 # Update sourcebans-pp
 git clone https://github.com/sbpp/sourcebans-pp.git
 cp -r sourcebans-pp/game/addons/sourcemod/scripting doi/addons/sourcemod
@@ -62,7 +71,7 @@ cp -r sm-advertisements/addons/sourcemod/scripting doi/addons/sourcemod
 rm -rf sm-advertisements
 
 # Enable SQL admin plugins
-cd doi/addons/sourcemod/plugins
+pushd doi/addons/sourcemod/plugins
 mv disabled/admin-sql-threaded.smx .
 mv disabled/sql-admin-manager.smx .
 
@@ -80,3 +89,4 @@ do
         ../scripting/spcomp64 ../scripting/$scriptfile.sp
     fi
 done
+popd
